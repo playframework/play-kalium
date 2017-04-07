2b7a8a3bdd29b201bcf7cebfceb13146a68efeaa
Will Sargent <will.sargent@gmail.com>
2017-04-07 16:23:08 -0700

    fix tests

--- test/services/encryption/UserInfoServiceSpec.scala
+++ test/services/encryption/UserInfoServiceSpec.scala
@@ -2,16 +2,18 @@ package services.encryption
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import services.user.{UserInfo, UserInfoService}
 
 class UserInfoServiceSpec extends PlaySpec with GuiceOneAppPerTest {
 
   "encryption info service" should {
 
     "symmetrically encrypt data" in {
-      val service = app.injector.instanceOf(classOf[DefaultEncryptionService])
-      val encryptedMap = service.encrypt(UserInfo(terriblePerson = true))
-      val decryptedUserInfo = service.decrypt(encryptedMap)
-      decryptedUserInfo.terriblePerson mustBe true
+      val service = app.injector.instanceOf(classOf[UserInfoService])
+      val secretKey = service.newSecretKey
+      val encryptedMap = service.encrypt(secretKey, Option(UserInfo(username = "will")))
+      val decryptedUserInfo = service.decrypt(secretKey, encryptedMap)
+      decryptedUserInfo mustBe Some(UserInfo("will"))
     }
 
   }
